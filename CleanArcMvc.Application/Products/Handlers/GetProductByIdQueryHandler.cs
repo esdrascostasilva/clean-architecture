787235024f4c86ad85e704004404b89d3640aa78
esdrascosta <esdras.cts@outlook.com>
2024-04-12 09:51:33 -0300

    [feat] Classes Handlers de consulta criadas.

---
+++ CleanArcMvc.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using CleanArcMvc.Application.Products.Queries;
+using CleanArcMvc.Domain.Entities;
+using CleanArcMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArcMvc.Application.Products.Handlers;
+
+public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+{
+    private readonly IProductRepository _productRepository;
+
+    public GetProductByIdQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _productRepository.GetProductByIdAsync(request.Id);
+    }
+}
